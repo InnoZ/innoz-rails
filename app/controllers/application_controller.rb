5a2ca2474fdb3967ce632d194326d8ff53edaae4
00BOCK <benno.bock@innoz.de>
2017-05-31 11:17:06 +0200

    adjusting to current layout and adding language switch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :set_locale
+
+  private
+
+  def browser_language
+    language_header = request.env['HTTP_ACCEPT_LANGUAGE']
+    language_header.scan(/^[a-z]{2}/).first.to_sym if language_header
+  end
+
+  def supported_browser_language
+    browser_language if I18n.available_locales.include?(browser_language)
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || supported_browser_language || I18n.default_locale
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
 end
